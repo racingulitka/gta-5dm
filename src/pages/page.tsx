56b38f47af653f7e891da2a27a8658a634d4af39
racingulitca <mex83011@gmail.com>
2024-01-14 12:00:26 +0300

    first commit

--- src/pages/page.tsx
+++ src/pages/page.tsx
@@ -15,9 +15,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        
+        111
       </main>
     </>
   )
 }
-///использоваать image для карттинок
