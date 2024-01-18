83a97ed42216fba747302ecf689194883912174d
racingulitca <mex83011@gmail.com>
2024-01-18 21:24:23 +0300

    feat:socialMediaComponent complete

--- src/components/SocialMedia/MediaButton/MediaButton.tsx
+++ src/components/SocialMedia/MediaButton/MediaButton.tsx
@@ -2,19 +2,60 @@ import React from 'react'
 import styles from './MediaButton.module.scss'
 import { StaticImageData } from 'next/image'
 import Image from 'next/image'
+import cn from 'classnames'
 
 const MediaButton = ({
+    id,
     icon,
+    iconBorder,
+    iconBoxShadow,
+    name,
+    color,
+    textShadow,
+    textBackground
 }: {
+    id:number
     icon: StaticImageData
+    iconBorder:string
+    iconBoxShadow:string
+    name:string
+    color:string
+    textShadow:string
+    textBackground:string
 }) => {
+
+    const numberWithSpaces = (number:number) => {
+        return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ')
+      }
+    const followers = numberWithSpaces(55555)
+
+    const iconAlign = id === 1 ? '0px' : '-1px'
+
     return (
         <div className={styles.wrapper}>
             <div className={styles.flexContainer}>
                 <div className={styles.iconContainer}>
-                    <div className={styles.background}>
-                        <Image src={icon} alt='image' className={styles.icon} />
+                    <div className={styles.background} style={{border:iconBorder, boxShadow:iconBoxShadow}}>
+                        <Image src={icon} alt='image' className={styles.icon} style={{transform:`translate(0, ${iconAlign})`}} />
                     </div>
+                </div>
+                <div className={styles.textBlock}>
+                    {
+                        color === '' ?
+                        <div
+                            style={{backgroundImage:textBackground, textShadow}}
+                            className={cn(styles.name, styles.nameBack)}
+                        >
+                                {name.toUpperCase()}
+                        </div>:
+                        <div
+                            className={cn(styles.name)}
+                            style={{color, textShadow }}
+                        >
+                                {name.toUpperCase()}
+                        </div>
+                    }
+                   <div className={styles.followers}>{followers} <span>подписчиков</span></div>
                 </div>
             </div>
         </div>
--- src/components/SocialMedia/SocialMedia.tsx
+++ src/components/SocialMedia/SocialMedia.tsx
@@ -12,10 +12,22 @@ import vkBackground from './assets/vkBackground.png'
 const SocialMedia = () => {
     return (
         <div className={styles.wrapper}>
-            <Image src={tiktokBackground} alt='tiktokBackground' className={styles.tiktokBackground} />
-            <Image src={youtubeBackground} alt='youtubeBackground' className={styles.youtubeBackground} />
-            <Image src={telegramBackground} alt='telegramBackground' className={styles.telegramBackground} />
-            <Image src={vkBackground} alt='vkBackground' className={styles.vkBackground} />
+            <div className={styles.tiktokBackgroundBlock}>
+                <div className={styles.tiktokBackgroundCover}></div>
+                <Image src={tiktokBackground} alt='tiktokBackground' className={styles.tiktokBackground} />
+            </div>
+            <div className={styles.youtubeBackgroundBlock}>
+                <div className={styles.youtubeBackgroundCover}></div>
+                <Image src={youtubeBackground} alt='youtubeBackground' className={styles.youtubeBackground} />
+            </div>
+            <div className={styles.telegramBackgroundBlock}>
+                <div className={styles.telegramBackgroundCover}></div>
+                <Image src={telegramBackground} alt='telegramBackground' className={styles.telegramBackground} />
+            </div>
+            <div className={styles.vkBackgroundBlock}>
+                <div className={styles.vkBackgroundCover}></div>
+                <Image src={vkBackground} alt='vkBackground' className={styles.vkBackground} />
+            </div>
             <div className={styles.content}>
                 <h2 className={styles.title}>Следи за нами и получайте ценные призы!</h2>
                 <p className={styles.description}>В наших соц сетях мы часто публикуем промокоды, проводим конкурсы. разыгрываем подарки, и многое другое.</p>
@@ -24,7 +36,15 @@ const SocialMedia = () => {
                         mediaButtonsArr.map(item => {
                             return (
                                 <MediaButton
+                                    key={item.id}
+                                    id={item.id}
                                     icon={item.icon}
+                                    iconBorder={item.iconBorder}
+                                    iconBoxShadow={item.iconBoxShadow}
+                                    name={item.name}
+                                    color={item.color}
+                                    textShadow={item.textShadow}
+                                    textBackground={item.textBackground}
                                 />
                             )
                         })
